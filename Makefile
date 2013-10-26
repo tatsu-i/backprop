5b49f3932dd41ae6ffcc666cfc3b7e2781f902cb
tatsui <tatsuya.ito.at@gmail.com>
2013-10-26 23:22:29 +0900

    2013年 10月 26日 土曜日 23:22:29 JST

--- Makefile
+++ Makefile
@@ -3,11 +3,14 @@ CFLAGS        = -g #-O4 -Wall -I/usr/local/include
 LDFLAGS       = #-L/usr/local/lib
 LIBS          = -lm
 OBJS          = main.o connectionist.o
-PROGRAM       = run
+PROGRAM       = backprop
 
 all:            $(PROGRAM)
 
 $(PROGRAM):     $(OBJS)
 	                $(CC) $(OBJS) $(LDFLAGS) $(LIBS) -o $(PROGRAM)
+
+run:$(PROGRAM)
+	./$(PROGRAM)
 
 clean:;         rm -f *.o *~ $(PROGRAM)
--- main.c
+++ main.c
@@ -8,29 +8,29 @@ main(int argc, char* argv[]) {
 	Init();//重みを+-0.5の乱数で初期化
 	unsigned int i = 0;
 
-	printf("XORの学習\n");
-	printf("======== start ========\n");
+	printf("\x1b[35mXORの学習\n\x1b[36m");
+	printf("======== start ========\n\x1b[32m");
 	while(1){
 		Input();
 		Forward();
 		BackProp();
 
 		//結果を表示
-		printf("%d回目\n", ++i);
+		printf("学習回数: \x1b[31m%d\x1b[32m回目\n", ++i);
 		nt.X[0]=0.0;nt.X[1]=0.0;Forward();
-		printf("X[0]=%lf,X[1]=%lf,O[0]=%.4lf\n", nt.X[0],nt.X[1],nt.O[0]);
+		printf("X[0]=%lf,X[1]=%lf,O[0]=\x1b[31m%.4lf\x1b[32m\n", nt.X[0],nt.X[1],nt.O[0]);
 		nt.X[0]=0.0;nt.X[1]=1.0;Forward();
-		printf("X[0]=%lf,X[1]=%lf,O[0]=%.4lf\n", nt.X[0],nt.X[1],nt.O[0]);
+		printf("X[0]=%lf,X[1]=%lf,O[0]=\x1b[31m%.4lf\x1b[32m\n", nt.X[0],nt.X[1],nt.O[0]);
 		nt.X[0]=1.0;nt.X[1]=0.0;Forward();
-		printf("X[0]=%lf,X[1]=%lf,O[0]=%.4lf\n", nt.X[0],nt.X[1],nt.O[0]);
+		printf("X[0]=%lf,X[1]=%lf,O[0]=\x1b[31m%.4lf\x1b[32m\n", nt.X[0],nt.X[1],nt.O[0]);
 		nt.X[0]=1.0;nt.X[1]=1.0;Forward();
-		printf("X[0]=%lf,X[1]=%lf,O[0]=%.4lf\n", nt.X[0],nt.X[1],nt.O[0]);
+		printf("X[0]=%lf,X[1]=%lf,O[0]=\x1b[31m%.4lf\x1b[32m\n", nt.X[0],nt.X[1],nt.O[0]);
 		if (CheckError() < EPS)
 			break;
 		else
 			printf("\x1b[5A");
 	}
-	printf("======== stop ========\n");
+	printf("\x1b[36m======== stop ========\n");
 	return 0;
 }
 
